3ef4bea2b9669b0bbe3cbb0fb9c04e1121bd508f
Semih Ayduran <semihayduran77@hotmail.com>
2025-05-13 11:51:43 +0300

    soru.dart ve test_veri.dart dosyaları eklendi

---
+++ lib/test_veri.dart
@@ -0,0 +1,43 @@
+import 'package:bilgi_testi/soru.dart';
+
+
+class TestVeri {
+  int _soruIndex = 0;
+
+  List<Soru> _soruBankasi = [
+    Soru(
+      soruMetni: '1.Titanic gelmiş geçmiş en büyük gemidir',
+      soruYaniti: false,
+    ),
+    Soru(
+      soruMetni: '2.Dünyadaki tavuk sayısı insan sayısından fazladır',
+      soruYaniti: true,
+    ),
+    Soru(soruMetni: '3.Kelebeklerin ömrü bir gündür', soruYaniti: false),
+    Soru(soruMetni: '4.Dünya düzdür', soruYaniti: false),
+    Soru(
+      soruMetni: '5.Kaju fıstığı aslında bir meyvenin sapıdır',
+      soruYaniti: true,
+    ),
+    Soru(
+      soruMetni: '6.Fatih Sultan Mehmet hiç patates yememiştir',
+      soruYaniti: true,
+    ),
+  ];
+
+  String getSoruMetni() {
+    return _soruBankasi[_soruIndex].soruMetni;
+  }
+
+  bool getSoruYaniti() {
+    return _soruBankasi[_soruIndex].soruYaniti;
+  }
+
+  void sonrakiSoru() {
+    if (_soruIndex + 1 < _soruBankasi.length) {
+      _soruIndex++;
+    }
+  }
+}
+
+
